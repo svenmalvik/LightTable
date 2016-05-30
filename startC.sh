40df90e0d0ae3dada81ee777cdf7277d828f1893
Sven Malvik <sven@malvik.de>
2016-05-30 22:17:35 +0200

    download lt

--- startC.sh
+++ startC.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker build --rm -t lighttable .
-docker run -it --name LightTable --rm -v $PWD:/home/LightTable lighttable bash
+docker run -it --name LightTable --rm -v $PWD:/home/LightTable svenmalvik/lighttable bash
