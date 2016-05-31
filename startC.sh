0827e411549b963068e763c467fb97c279e04bcf
Sven Malvik <Sven.Malvik@husbanken.no>
2016-05-31 14:30:21 +0200

    dev start

--- startC.sh
+++ startC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name LightTable --rm -v $PWD:/home/LightTable -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable
+docker run -it --name LightTable --rm -v $PWD:/home/LightTable -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable bash
