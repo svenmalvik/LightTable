b885c09d6dd5ecd44e67053d7d62ff0bee158fd2
Sven Malvik <Sven.Malvik@husbanken.no>
2016-06-03 14:17:18 +0200

    .

--- startC.sh
+++ startC.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --name LightTable --rm -v $PWD:/home/LightTable -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable
+LT_CONFIG=$HOME/LightTable/.config
+
+[ ! -d $LT_CONFIG ] && mkdir -p $LT_CONFIG
+docker run --name LightTable --rm -v /home/hosm:/home -v $LT_CONFIG:/root/.config  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable
--- startCdev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --name LightTable --rm -v $PWD:/home/LightTable -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable bash
