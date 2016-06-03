deedfabb67215bdf0888c3aacf1c5c6a76233b95
Sven Malvik <Sven.Malvik@husbanken.no>
2016-06-03 14:43:36 +0200

    .

--- startC.sh
+++ startC.sh
@@ -3,4 +3,4 @@
 LT_CONFIG=$HOME/LightTable/.config
 
 [ ! -d $LT_CONFIG ] && mkdir -p $LT_CONFIG
-docker run --name LightTable --rm -v /home/hosm:/home -v $LT_CONFIG:/root/.config  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable
+docker run -it --name LightTable --rm -v $HOME:$HOME -v $LT_CONFIG:/root/.config -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY svenmalvik/lighttable bash
